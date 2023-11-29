aa48f92e51801986a4f35cea65d3498ec3f71cea
Jonas Kauker <jkauker@2-A-4.42heilbronn.de>
2023-11-29 14:54:46 +0100

    starter files

---
+++ main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 14:40:08 by jkauker           #+#    #+#             */
+/*   Updated: 2023/11/29 14:54:22 by jkauker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		log_error();
+		return (1);
+	}
+	if (argc == 2)
+	{
+		// when args in one string
+		// split it with ft_split
+	}
+	else
+	{
+		// when args not in one string
+	}
+	return (0);
+}
---
+++ pushswap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 14:42:33 by jkauker           #+#    #+#             */
+/*   Updated: 2023/11/29 14:47:01 by jkauker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+
+# include <unistd.h>
+
+# define ERROR "Error\n"
+
+void	log_error(void);
+
+#endif
