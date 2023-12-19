23f4507617a6f1af8c25f4c82be04da90939f1e0
reptudn <densetsu467@gmail.com>
2023-12-19 11:52:56 +0100

    updated rra and rrb

--- operations/rra.c
+++ operations/rra.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rra.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
+/*   By: jonask <jonask@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:08:53 by jkauker           #+#    #+#             */
-/*   Updated: 2023/12/15 14:15:31 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/12/19 11:27:43 by jonask           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,30 @@
 // rra : reverse rotate a - shift down all elements of stack a by 1. The last
 void	rra(long *stack_a, int *size, int print)
 {
-	long	tmp;
-	int		i;
-	int		j;
+	long tmp;
+    int i, j;
 
-	tmp = stack_a[*size - 1];
-	i = *size - 1;
-	j = *size - 1;
-	while (i > 0)
-	{
-		if (stack_a[i - 1] != NOTHING)
-			stack_a[j--] = stack_a[i - 1];
-		i--;
-	}
-	if (tmp != NOTHING)
-		stack_a[j--] = tmp;
-	while (j >= 0)
-		stack_a[j--] = NOTHING;
-	if (print)
-		write(1, "rra\n", 4);
+    tmp = stack_a[*size - 1];
+    i = *size - 1;
+    j = *size - 1;
+
+    while (i > 0) {
+        stack_a[i] = stack_a[i - 1];
+        i--;
+    }
+
+    stack_a[0] = tmp;
+
+    // Move all NOTHING values to the bottom of the stack
+    for (i = 0; i < *size; i++) {
+        if (stack_a[i] == NOTHING) {
+            for (j = i; j < *size - 1; j++) {
+                stack_a[j] = stack_a[j + 1];
+            }
+            stack_a[*size - 1] = NOTHING;
+        }
+    }
+
+    if (print)
+        write(1, "rra\n", 4);
 }
--- operations/rrb.c
+++ operations/rrb.c
@@ -15,23 +15,30 @@
 // rrb : reverse rotate b - shift down all elements of stack b by 1. The last
 void	rrb(long *stack_b, int *size, int print)
 {
-	long	tmp;
-	int		i;
-	int		j;
+	long tmp;
+    int i, j;
 
-	tmp = stack_b[*size - 1];
-	i = *size - 1;
-	j = *size - 1;
-	while (i > 0)
-	{
-		if (stack_b[i - 1] != NOTHING)
-			stack_b[j--] = stack_b[i - 1];
-		i--;
-	}
-	if (tmp != NOTHING)
-		stack_b[j--] = tmp;
-	while (j >= 0)
-		stack_b[j--] = NOTHING;
-	if (print)
-		write(1, "rrb\n", 4);
+    tmp = stack_b[*size - 1];
+    i = *size - 1;
+    j = *size - 1;
+
+    while (i > 0) {
+        stack_b[i] = stack_b[i - 1];
+        i--;
+    }
+
+    stack_b[0] = tmp;
+
+    // Move all NOTHING values to the bottom of the stack
+    for (i = 0; i < *size; i++) {
+        if (stack_b[i] == NOTHING) {
+            for (j = i; j < *size - 1; j++) {
+                stack_b[j] = stack_b[j + 1];
+            }
+            stack_b[*size - 1] = NOTHING;
+        }
+    }
+
+    if (print)
+        write(1, "rrb\n", 4);
 }
